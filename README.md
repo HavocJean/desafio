bea42fcd731b2b3d9bfd684409a6d0a19c7bb2c3
Jean <jean.magic@hotmail.com>
2020-10-29 09:28:23 -0300

    Arrumado botao e acrescentado contador, removido comentarios

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ uma tabela no mysql com os cadastros dos clientes
 
 criado filtros através de GET no front end e utilizando função no laravel para buscar apenas o filtro desejado
 conforme o filtro mostrará apenas aquilo que foi selecionado
+além de um search que pode buscar clientes pelo nome
--- app/Http/Controllers/ClientesController.php
+++ app/Http/Controllers/ClientesController.php
@@ -10,16 +10,12 @@ class ClientesController extends Controller
 {
     public function index()
     {
-
         return view('cliente');
-
     }
 
     public function create(Request $r)
     {
-            // dd($r->all());
-            // $cliente = New Clientes();
-            // $cliente->create($r->except(['token']));
+        // cadastra um cliente novo
         $cliente = Clientes::create([
             'nome_usuario' => $r['nome_usuario'],
             'altura' => preg_replace('/[,]+/','.',$r['altura']),
@@ -33,68 +29,71 @@ class ClientesController extends Controller
 
     public function todo(Request $r)
     {
+        // conta qts clientes existem cadastrados
+        $contador = Clientes::count();
         // filtro peso
         if (request()->has('peso')) {
             if(request('peso') == "acima") {
                 $clientes = Clientes::where('peso', '>', '90')->get();
 
-                return view('filtro', ['clientes' => $clientes]);
+                return view('filtro', ['clientes' => $clientes, 'contador' => $contador]);
 
             } elseif(request('peso') == "abaixo") {
                 $clientes = Clientes::where('peso', '<', '69')->get();
 
-                return view('filtro', ['clientes' => $clientes]);
+                return view('filtro', ['clientes' => $clientes, 'contador' => $contador]);
 
             } else {
                 $clientes = Clientes::where('peso', '>', '70')->where('peso', '<', '89')->get();
                 
-                return view('filtro', ['clientes' => $clientes]);
+                return view('filtro', ['clientes' => $clientes, 'contador' => $contador]);
             } // filtro altura
         } elseif (request()->has('altura')) {
             if(request('altura') == "altos") { 
                 $clientes = Clientes::where('altura', '>', '1.80')->get();
 
-                return view('filtro', ['clientes' => $clientes]);
+                return view('filtro', ['clientes' => $clientes, 'contador' => $contador]);
 
             } elseif(request('altura') == "medianos") {
                 $clientes = Clientes::where('altura', '>', '1.60')->where('peso', '<', '1.79')->get();
 
-                return view('filtro', ['clientes' => $clientes]);
+                return view('filtro', ['clientes' => $clientes, 'contador' => $contador]);
 
             } else {
                 $clientes = Clientes::where('altura', '<', '1.59')->get();
 
-                return view('filtro', ['clientes' => $clientes]);
+                return view('filtro', ['clientes' => $clientes, 'contador' => $contador]);
             } // filtro lactose
         } elseif (request()->has('lactose')) {
             if(request('lactose') == "sim") { 
                 $clientes = Clientes::where('lactose','=', '1')->get();
 
-                return view('filtro', ['clientes' => $clientes]);
+                return view('filtro', ['clientes' => $clientes, 'contador' => $contador]);
 
             } elseif(request('lactose') == "nao") {
                 $clientes = Clientes::where('lactose', '=', '0')->get();
 
-                return view('filtro', ['clientes' => $clientes]);
+                return view('filtro', ['clientes' => $clientes, 'contador' => $contador]);
             } // filtro atleta
         } elseif (request()->has('atleta')) {
             if(request('atleta') == "sim") { 
                 $clientes = Clientes::where('atleta','=', '1')->get();
 
-                return view('filtro', ['clientes' => $clientes]);
+                return view('filtro', ['clientes' => $clientes, 'contador' => $contador]);
 
             } elseif(request('atleta') == "nao") {
                 $clientes = Clientes::where('atleta', '=', '0')->get();
 
-                return view('filtro', ['clientes' => $clientes]);
+                return view('filtro', ['clientes' => $clientes, 'contador' => $contador]);
             } // search para buscar nome do usuario
         } elseif (request()->has('busca')) { 
             $clientes = Clientes::where('nome_usuario','LIKE', $r['busca'])->get();
 
-            return view('filtro', ['clientes' => $clientes]);
+            return view('filtro', ['clientes' => $clientes, 'contador' => $contador]);
         } else {
-            $clientes = Clientes::simplePaginate(8);
-            return view('filtro', ['clientes' => $clientes]);
+            $clientes = Clientes::simplePaginate(20);
+
+            return view('filtro', ['clientes' => $clientes, 'contador' => $contador]);
         }
     }
 }
